30d051bbf0aaf34091b05f88848d92d4c0b6dc2b
DEVQUEEN\genes <genesismojeda@gmail.com>
2024-05-15 14:50:00 -0400

    member id schema

--- project_management_front/src/forms/tasks/SchemaValidation.tsx
+++ project_management_front/src/forms/tasks/SchemaValidation.tsx
@@ -4,7 +4,7 @@ import { z } from 'zod';
 const taskSchema = z.object({
   task_name: z.string().min(1, { message: "Task name is required." }),
   project_id: z.number().min(1, { message: "Task name is required." }),
-  member_id: z.string().min(1, { message: "Member od is required." }),
+  member_id: z.string().min(1, { message: "Member id is required." }),
   start_date: z.string()
     .optional(),
   end_date: z.string()
